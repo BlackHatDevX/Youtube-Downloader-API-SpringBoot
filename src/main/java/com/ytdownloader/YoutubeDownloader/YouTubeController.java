069c1f6e70aeb75b1e5627153cd37d79166d799b
blackhatdevx <jashgro@yandex.com>
2024-11-25 23:53:54 +0530

    patch

--- src/main/java/com/ytdownloader/YoutubeDownloader/YouTubeController.java
+++ src/main/java/com/ytdownloader/YoutubeDownloader/YouTubeController.java
@@ -1,75 +1,90 @@
 package com.ytdownloader.YoutubeDownloader;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/api/youtube")
 public class YouTubeController {
-
     private static final String DOWNLOAD_DIR = "downloads/";
 
-    // Endpoint to download video and return a link to the file
+    // endpoint to download video and return the file link
     @GetMapping("/download")
     public ResponseEntity<String> downloadVideo(@RequestParam String url) {
         try {
-            // Ensure the download directory exists
+            // CREATE DOWNLOAD DIRECTORY
             Files.createDirectories(Path.of(DOWNLOAD_DIR));
 
-            // Generate yt-dlp command
+            // FINALIZE COMMAND TO DOWNLOAD YOUTUBE VIDEO WITH THE HELP OF YT-DLP
             String command = String.format("yt-dlp -o %s%%(title)s.%%(ext)s %s", DOWNLOAD_DIR, url);
-
-            // Execute the command
+            
+            // EXECUTE THE COMMAND IN RUNTIME
             Process process = Runtime.getRuntime().exec(command);
-            process.waitFor();
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                String errorMessage = new String(process.getErrorStream().readAllBytes());
+                return ResponseEntity.status(500).body("Error occurred while downloading: " + errorMessage);
+            }
+            
+            
 
-            // Get the downloaded file
+            // List all the files in download folder
             File[] files = new File(DOWNLOAD_DIR).listFiles();
-            if (files != null && files.length > 0) {
-                File videoFile = files[0]; // Assuming the first file is the downloaded video
 
-                // Replace spaces with dashes in the file name
+            // first file is the downloaded file
+            if (files != null && files.length>0){
+                File videoFile = files[0];
+                
+                // Fix the name of file to remove special characters
                 String originalFileName = videoFile.getName();
-                String modifiedFileName = originalFileName.replaceAll("[^a-zA-Z0-9.]", "");
-                File modifiedFile = new File(DOWNLOAD_DIR + modifiedFileName);
-                if (!originalFileName.equals(modifiedFileName)) {
-                    videoFile.renameTo(modifiedFile);
+                String modifiedFileName = originalFileName.replaceAll("[^a-zA-Z0-9.]", "_");
+                File modifiedFile = new File(DOWNLOAD_DIR, modifiedFileName);
+                if (!originalFileName.equals(modifiedFileName) && modifiedFile.exists()) {
+                    modifiedFileName = System.currentTimeMillis() + "_" + modifiedFileName;
+                    modifiedFile = new File(DOWNLOAD_DIR, modifiedFileName);
                 }
+                videoFile.renameTo(modifiedFile);
+                
 
-                // Return the link to the file
-                String fileLink = String.format("http://localhost:8080/api/youtube/file/%s", modifiedFileName);
-                return ResponseEntity.ok("Download successful! File link: " + fileLink);
+                // return the link to file
+                String fileLink = String.format("http://localhost:8080/api/youtube/file/%s",modifiedFileName);
+                return ResponseEntity.ok("Click here to download video : <a href="+fileLink+">"+originalFileName+"</a>");
             }
 
-            return ResponseEntity.status(404).body("No video was downloaded.");
+            // Print that the file is downloaded
+            return( ResponseEntity.status(404).body("Error: Video isn't downloaded"));
+            
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("An error occurred: " + e.getMessage());
+            return ResponseEntity.status(500).body("An error occured :"+ e.getMessage());
         }
     }
 
-    // Endpoint to serve the downloaded file
     @GetMapping("/file/{fileName}")
-    public ResponseEntity<Resource> serveFile(@PathVariable String fileName) {
+    public ResponseEntity serveFile(@PathVariable String fileName) {
         try {
-            File file = new File(DOWNLOAD_DIR + fileName);
-            if (file.exists()) {
+            File file = new File(DOWNLOAD_DIR+fileName);
+            if (!fileName.matches("[a-zA-Z0-9._-]+")) {
+                return ResponseEntity.status(400).body("Invalid file name");
+            }
+            
+            if (file.exists()){
                 Resource resource = new FileSystemResource(file);
-                return ResponseEntity.ok()
-                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
-                        .body(resource);
-            } else {
-                return ResponseEntity.status(404).body(null);
+                return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+file.getName()+"\"").body(resource);
             }
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(null);
+            return ResponseEntity.status(500).body("Exception Error : "+e);
         }
+        return ResponseEntity.ok("ok");
     }
+    
+    
+    
 }
